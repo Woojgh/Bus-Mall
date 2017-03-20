87a28f28f36eb9dd6faa6c101821d9bd049596a6
James Salamonsen <jamessalamonsen@gmail.com>
2017-03-20 14:26:31 -0700

    got my html set up and working on my app.js.

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function TestItem(itemName, itemPath) {
+	this.itemName = itemName;
+	this.itemPath = itemPath;
+	itemListArray.push(this);
+}
+
+itemListArray = [];
+oldListItemsArray = [];
+newListItemsArray = [];
+
+function randomItemList() {
+	Math.floor(Math.random() * (itemsListArray.length + 1));
+}
+var randomItemListPic = randomItemList();
+
+
+var = new TestItem ('bag', 'assets/bag.jpg');
+var = new TestItem ('banana', 'assets/banana.jpg');
+var = new TestItem ('bathroom', 'assets/bathroom.jpg');
+var = new TestItem ('boots', 'assets/boots.jpg', );
+var = new TestItem ('breakfast', 'assets/breakfast.jpg');
+var = new TestItem ('bubblegum', 'assets/bubblegum.jpg');
+var = new TestItem ('chair', 'assets/chair.jpg');
+var = new TestItem ('chtulu', 'assets/cthulhu.jpg');
+var = new TestItem ('dog-duck', 'assets/dog-duck.jpg');
+var = new TestItem ('dragon', 'assets/dragon.jpg');
+var = new TestItem ('pen', 'assets/pen.jpg');
+var = new TestItem ('pet-sweep', 'assets/pet-sweep.jpg');
+var = new TestItem ('scissors', 'assets/scissors.jpg');
+var = new TestItem ('shark', 'assets/shark.jpg');
+var = new TestItem ('sweep', 'assets/sweep.png');
+var = new TestItem ('tauntaun', 'assets/tauntaun.jpg');
+var = new TestItem ('unicorn', 'assets/unicorn.jpg');
+var = new TestItem ('usb', 'assets/usb.gif');
+var = new TestItem ('water-can', 'assets/water-can.jpg');
+var = new TestItem ('wine glass', 'assets/wine-glass.jpg');
