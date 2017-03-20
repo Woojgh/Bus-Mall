478f2f504996c77f636b6fe44307d3873f3e125b
James Salamonsen <jamessalamonsen@gmail.com>
2017-03-20 14:52:47 -0700

    Addded my three images generation function.

--- app.js
+++ app.js
@@ -10,29 +10,31 @@ itemListArray = [];
 oldListItemsArray = [];
 newListItemsArray = [];
 
-function randomItemList() {
+function randomItemListFunc() {
 	Math.floor(Math.random() * (itemsListArray.length + 1));
 }
-var randomItemListPic = randomItemList();
+var randomItemListPic = randomItemListFunc();
+for (var i = 0; i < 3; i++) {
+	newListItemsArray.push(randomItemListPic);
+}
 
-
-var = new TestItem ('bag', 'assets/bag.jpg');
-var = new TestItem ('banana', 'assets/banana.jpg');
-var = new TestItem ('bathroom', 'assets/bathroom.jpg');
-var = new TestItem ('boots', 'assets/boots.jpg', );
-var = new TestItem ('breakfast', 'assets/breakfast.jpg');
-var = new TestItem ('bubblegum', 'assets/bubblegum.jpg');
-var = new TestItem ('chair', 'assets/chair.jpg');
-var = new TestItem ('chtulu', 'assets/cthulhu.jpg');
-var = new TestItem ('dog-duck', 'assets/dog-duck.jpg');
-var = new TestItem ('dragon', 'assets/dragon.jpg');
-var = new TestItem ('pen', 'assets/pen.jpg');
-var = new TestItem ('pet-sweep', 'assets/pet-sweep.jpg');
-var = new TestItem ('scissors', 'assets/scissors.jpg');
-var = new TestItem ('shark', 'assets/shark.jpg');
-var = new TestItem ('sweep', 'assets/sweep.png');
-var = new TestItem ('tauntaun', 'assets/tauntaun.jpg');
-var = new TestItem ('unicorn', 'assets/unicorn.jpg');
-var = new TestItem ('usb', 'assets/usb.gif');
-var = new TestItem ('water-can', 'assets/water-can.jpg');
-var = new TestItem ('wine glass', 'assets/wine-glass.jpg');
+var = new TestItem ('bag', 'images/bag.jpg');
+var = new TestItem ('banana', 'images/banana.jpg');
+var = new TestItem ('bathroom', 'images/bathroom.jpg');
+var = new TestItem ('boots', 'images/boots.jpg', );
+var = new TestItem ('breakfast', 'images/breakfast.jpg');
+var = new TestItem ('bubblegum', 'images/bubblegum.jpg');
+var = new TestItem ('chair', 'images/chair.jpg');
+var = new TestItem ('chtulu', 'images/cthulhu.jpg');
+var = new TestItem ('dog-duck', 'images/dog-duck.jpg');
+var = new TestItem ('dragon', 'images/dragon.jpg');
+var = new TestItem ('pen', 'images/pen.jpg');
+var = new TestItem ('pet-sweep', 'images/pet-sweep.jpg');
+var = new TestItem ('scissors', 'images/scissors.jpg');
+var = new TestItem ('shark', 'images/shark.jpg');
+var = new TestItem ('sweep', 'images/sweep.png');
+var = new TestItem ('tauntaun', 'images/tauntaun.jpg');
+var = new TestItem ('unicorn', 'images/unicorn.jpg');
+var = new TestItem ('usb', 'images/usb.gif');
+var = new TestItem ('water-can', 'images/water-can.jpg');
+var = new TestItem ('wine glass', 'images/wine-glass.jpg');
